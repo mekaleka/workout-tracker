03c97f14f1e6a0af34b61e6aec62e127c64f4c96
mekaleka <www.no-reply.com>
2020-03-25 20:59:39 -0700

    installd npm packages/.gitignore

--- server.js
+++ server.js
@@ -16,3 +16,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
+
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
